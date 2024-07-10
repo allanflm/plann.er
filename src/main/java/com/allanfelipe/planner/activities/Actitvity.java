823d1a90fe2154f13def43c43af02151414f7ac4
Allan <allanfelipedk@gmail.com>
2024-07-10 19:27:12 -0300

    feat: Create a table and an entity that will represent an activity

---
+++ src/main/java/com/allanfelipe/planner/activities/Actitvity.java
@@ -0,0 +1,31 @@
+package com.allanfelipe.planner.activities;
+
+import com.allanfelipe.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "activities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Actitvity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "occurs_at", nullable = false)
+    private LocalDateTime occursAt;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
